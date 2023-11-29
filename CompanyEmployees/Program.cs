8be144d35aa65cf676549ad15cc6a6c8ac513ccb
Kelbin <93187893+kelbindev@users.noreply.github.com>
2023-11-29 22:48:36 +0800

    configure DI

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,14 +1,26 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage(); 
+else app.UseHsts();
 
+// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
+
+app.UseStaticFiles(); 
+app.UseForwardedHeaders(new ForwardedHeadersOptions { ForwardedHeaders = ForwardedHeaders.All }); 
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
