77d7c74bfec7ffeee8db6f3561ccbc54214c3630
Kelbin <93187893+kelbindev@users.noreply.github.com>
2023-12-02 19:31:51 +0800

    Added Service

---
+++ Service/EmployeeService.cs
@@ -0,0 +1,15 @@
+﻿using Contracts;
+using Service.Contracts;
+
+namespace Service;
+internal sealed class EmployeeService : IEmployeeService
+{
+    private readonly IRepositoryManager _repositoryManager;
+    private readonly ILoggerManager _loggerManager;
+
+    public EmployeeService(IRepositoryManager repositoryManager, ILoggerManager loggerManager)
+    {
+        _repositoryManager= repositoryManager;
+        _loggerManager= loggerManager;
+    }
+}
