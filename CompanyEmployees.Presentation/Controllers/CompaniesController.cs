f19acc92853bbe401cf8f5eff856849e7efb1fc5
Kelbin <93187893+kelbindev@users.noreply.github.com>
2023-12-03 00:36:22 +0800

    added get all companies in controller

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
 
 namespace CompanyEmployees.Presentation.Controllers;
 
@@ -6,5 +7,24 @@ namespace CompanyEmployees.Presentation.Controllers;
 [Route("api/[companies]")]
 public class CompaniesController : ControllerBase
 {
+    private readonly IServiceManager _service;
 
+    public CompaniesController(IServiceManager service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public IActionResult GetCompanies()
+    {
+        try
+        {
+            var companies = _service.CompanyService.GetAllCompanies(trackChanges: false);
+            return Ok(companies);
+        }
+        catch
+        {
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
 }
