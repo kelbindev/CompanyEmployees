d5d1493e79cb9e170aebd64e98e8d0c74399f68b
Kelbin <93187893+kelbindev@users.noreply.github.com>
2023-12-04 21:11:33 +0800

    update global error handling

--- CompanyEmployees/Extensions/GlobalExceptionHandler.cs
+++ CompanyEmployees/Extensions/GlobalExceptionHandler.cs
@@ -14,14 +14,18 @@ public class GlobalExceptionHandler : IExceptionHandler
     }
     public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
     {
-        httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError; 
         httpContext.Response.ContentType = "application/json"; 
         var contextFeature = httpContext.Features.Get<IExceptionHandlerFeature>(); 
         if (contextFeature != null) {
+            httpContext.Response.StatusCode = contextFeature.Error switch {
+                NotFoundException => StatusCodes.Status404NotFound, 
+                _ => StatusCodes.Status500InternalServerError 
+            };
+
             _logger.LogError($"Something went wrong: {exception.Message}"); 
             await httpContext.Response.WriteAsync(new ErrorDetails() { 
-                StatusCode = httpContext.Response.StatusCode, 
-                Message = "Internal Server Error.", 
+                StatusCode = httpContext.Response.StatusCode,
+                Message = contextFeature.Error.Message,
             }.ToString()); 
         }
         return true;
